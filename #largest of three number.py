c98927c2d3fb2602be67cff75c8b4155cbe67a1b
sinchumnaik841-lang <sinchumnaik841@gmail.com>
2025-11-12 14:09:56 +0530

    Add program to find the largest of three numbers
    Implement a program to find the largest of three numbers.

---
+++ #largest of three number.py
@@ -0,0 +1,8 @@
+# largest  of three number
+s=int(input("enter the first number"))
+p=int(input("enter the second number"))
+q=int(input("enter the third numbe"))+
+if s>=p and s>=q:
+  print("the largest number",s)
+  print("the largest number",p)
+  print("the largest number",q)
