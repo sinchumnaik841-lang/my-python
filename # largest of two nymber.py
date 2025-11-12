a8d4b66fffae0ce56458b23fdf33aef0951f8420
sinchumnaik841-lang <sinchumnaik841@gmail.com>
2025-11-12 14:08:14 +0530

    Create # largest of two nymber.py

---
+++ # largest of two nymber.py
@@ -0,0 +1,7 @@
+#largest of two number
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+if a<b:
+ print("largest number is",a)
+else:
+  print("largest number is",b)
