805c827d069c5c700e345329c2ee18e923d8d042
sinchumnaik841-lang <sinchumnaik841@gmail.com>
2025-11-12 14:13:57 +0530

    Create #divisibility test.py

---
+++ #divisibility test.py
@@ -0,0 +1,10 @@
+# divisibility test
+num=int(input("enter the number to checker: "))
+divisor=int(input)("enter the divisor:")
+if divisor==0:
+  print("divisor cannot be zero!")
+else:
+     if num%divisor==0:
+      print(f"{num} is divisible by {divisor}.")
+else:
+     print(f"f{num} is not divisible by {divisor}.")
