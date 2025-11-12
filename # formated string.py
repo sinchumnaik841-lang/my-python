1b7b8e14681906d3f93704fe06e7c9d559a6fd98
sinchumnaik841-lang <sinchumnaik841@gmail.com>
2025-11-12 12:59:27 +0530

    Add formatted string example in new file

---
+++ # formated string.py
@@ -0,0 +1,6 @@
+# formated string
+name= "sinchana"
+age = 18
+village = "arsikere"
+college = "shridevi"
+print(f"my name is :{name} and my age is: {age} village name: {village} college name is:{college}")
