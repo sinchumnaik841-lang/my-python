96b8f760a659d219400942323b3560fb78f362e7
sinchumnaik841-lang <sinchumnaik841@gmail.com>
2025-11-14 15:20:29 +0530

    Create #largest and smallest number list.py

---
+++ #largest and smallest number list.py
@@ -0,0 +1,6 @@
+#find the largest and smallest in list
+sinchu=[10,15,18,25,366,57,52]
+largest= max(sinchu)
+smallest= min(sinchu)
+print("largest element",largest)
+print("smallest element",smallest)
